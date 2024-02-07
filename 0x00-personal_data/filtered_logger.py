7b0d962f4a7d7235611b764e40d6d0f886eb3d2e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-07 19:40:26 +0100

    user_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,16 +22,15 @@ PII_FIELDS = ("username", "email", "password", "credit_card", "ssn")
 
 
 def get_logger() -> logging.Logger:
-    """Return a logging.Logger object named 'user_data'."""
+    """Returns a Logger Object"""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
-    # Create a StreamHandler
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
-
     logger.addHandler(stream_handler)
+
     return logger
 
 
