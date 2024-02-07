8b1034fe563df1e240dc9c22bb41133f50000fcd
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-07 19:42:58 +0100

    user_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-""" Personal data source file for the logs."""
-
+"""
+Module for handling Personal Data
+"""
 from typing import List
-import logging
-import os
 import re
+import logging
+from os import environ
+import mysql.connector
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
-    """returns the log message obfuscated."""
-    for field in fields:
-        pattern = f"{field}=.*?{separator}"
-        replacement = f"{field}={redaction}{separator}"
-        message = re.sub(pattern, replacement, message)
+    """Returns a log message obfuscated"""
+    for f in fields:
+        message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
     return message
-
-
-PII_FIELDS = ("username", "email", "password", "credit_card", "ssn")
 
 
 def get_logger() -> logging.Logger:
@@ -34,6 +34,39 @@ def get_logger() -> logging.Logger:
     return logger
 
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to a MySQL database"""
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(
+        user=username, password=password, host=host, database=db_name
+    )
+    return cnx
+
+
+def main():
+    """
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = "".join(f"{f}={str(r)}; " for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
+
+
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
 
@@ -42,13 +75,16 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """Initialize the RedactingFormatter object."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Return the formatted log message."""
+        """Filters values in incoming log records using filter_datum"""
         record.msg = filter_datum(
-            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
         )
-        return super().format(record)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == "__main__":
+    main()
