7186ed2c6584d7b28c2c147c64b02b5784b03a66
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-13 21:01:03 +0100

    user_data

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,10 +13,18 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if os.getenv("AUTH_TYPE") == "auth":
+auth = getenv("AUTH_TYPE")
+
+if auth == "auth":
     from api.v1.auth.auth import Auth
 
     auth = Auth()
+elif auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+
+    auth = BasicAuth()
+else:
+    auth = None
 
 
 @app.errorhandler(404)
@@ -38,17 +46,17 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before_request_func():
+def before_request():
     """Before request handler"""
-    if auth is None:
-        return
-    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth is not None:
+        if auth.require_auth(
+            request.path,
+            ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"],
+        ):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
