3682ac854c48aef0f2b9306efa8d6befadd7d74e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-15 14:14:24 +0100

    user_data

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,8 +15,7 @@ class SessionAuth(Auth):
         """Create a Session ID for a user_id"""
         if user_id is None or not isinstance(user_id, str):
             return None
-        if user_id not in User.users:
-            return None
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
